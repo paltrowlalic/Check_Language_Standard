2b6c2098d9011c7ee9ed67aa68d37a856ec20483
Afro <jiangnan.zhang@outlook.com>
2025-08-22 15:28:06 +0100

    Update to C++23 and improve standard detection
    Changed the Xcode project to use the C++23 standard. Enhanced main.cpp to detect and display the active C++ language standard, including support for future standards and improved output.

--- Check_Language_Standard/main.cpp
+++ Check_Language_Standard/main.cpp
@@ -7,8 +7,52 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const int numStandards = 7;
+
+const long stdCode[numStandards] = { 199711L, 201103L, 201402L, 201703L, 202002L, 202302L, 202612L};
+const char* stdName[numStandards] = { "Pre-C++11", "C++11", "C++14", "C++17", "C++20", "C++23", "C++26" };
+
+long getCPPStandard()
+{
+#if defined (_MSVC_LANG)
+    return _MSVC_LANG;
+#elif defined (_MSC_VER)
+    
+    return -1;
+#else
+    
+    return __cplusplus;
+#endif
+}
+
+int main()
+{
+    long standard = getCPPStandard();
+    
+    if (standard == -1)
+    {
+        std::cout << "Error: Unable to determine your language standard.  Sorry.\n";
+        return 0;
+    }
+    
+    for (int i = 0; i < numStandards; ++i)
+    {
+        
+        
+        if (standard == stdCode[i])
+        {
+            std::cout << "Your compiler is using " << stdName[i]
+            << " (language standard code " << standard << "L)\n";
+            break;
+        }
+        
+        if (standard < stdCode[i])
+        {
+            std::cout << "Your compiler is using a preview/pre-release of " << stdName[i]
+            << " (language standard code " << standard << "L)\n";
+            break;
+        }
+    }
+    
     return 0;
 }
