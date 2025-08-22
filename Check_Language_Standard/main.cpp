5bc13591c835e1d9536ca3d25ab4226373481bfc
Afro <jiangnan.zhang@outlook.com>
2025-08-22 15:01:30 +0100

    Initial Commit

---
+++ Check_Language_Standard/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Check_Language_Standard
+//
+//  Created by Jiangnan Zhang on 22/08/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
